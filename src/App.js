7832662240867ddb4755a991f45e4e4c8b2f6e4a
Ryan Santos <santosryan131@gmail.com>
2023-01-24 18:48:13 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import './scss/reset.scss';
+import './scss/all.scss';
+import Banner from './components/Banner';
+import Header from './components/Header';
+import About from './components/About';
+import Gallery from './components/Gallery';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Services from './components/Services';
+import Instagram from './components/Instagram';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Banner />
+      <Services />
+      <Instagram />
+      <About />
+      {/* <Gallery />
+      <Contact /> */}
+      <Footer />
     </div>
   );
 }
